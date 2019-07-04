ee0a2a476d441e6595eca2a431a6acab2c996d60
cherifabdelmajid <abdelmajidcherif26@gmail.com>
2019-07-04 12:32:19 +0200

    Todo list checkpoint

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
-import React from 'react';
-import Button from"./ImprovedButton"
+import React, { Component } from 'react';
+import './App.css';
+import List from './List';
 
-import ImprovedButton2 from "./ImprovedButton2"
-
-
-const person=[ 
-{
-    name:'sarra',
-    imgsrc:'sarrra.jpg',
-    description:'hffjfkroflfkf'
-},
-{
-    name:'Harold',
-    imgsrc:'Harold.jpg',
-    description:'trhytyyr'
-},
-{
-    name:'Daniel',
-    imgsrc:'Daniel.jpg',
-    description:'hfffififkd'
-}
-]
-    
-class App extends React.Component {
-    state= {
-      index:0
-    }
-  
-    render() {
-      return (<div>
-        <div>
-        {person.map((e, i) => <Button onClick={()=>this.setState({
-          index: i
-        
-        }) } name={e.name} />)
-  
-  
-    } </div>
-    <ImprovedButton2 
-    name={person[this.state.index].name} title={person[this.state.index].name} src={person[this.state.index].imgsrc} descri={person[this.state.index].description}
-    />
-     </div>)
-    }
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: [],
+      index: ""
+    };
   }
-  
-  export default App;
+
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+  Delete =(event) => {
+    this.setState({index: event.splice.List})
+  }  
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term],
+      
+    });
+  }
+ 
+  render() {
+    return (
+      <div>
+        <form className="App" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          
+          <button>Add</button>
+          
+        </form>
+
+        
+
+
+
+        <List style={{textDecoration:"line-through"}} items={this.state.items} />
+      </div>
+    );
+  }
+}
